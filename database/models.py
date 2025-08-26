f1fc1d3045686a094132cbc24c42ecf6c4188a6e
Lucas-Blanger <blangerlucas@gmail.com>
2025-08-26 15:15:47 -0300

    feat: rodando seed quando inicia o docker

--- database/models.py
+++ database/models.py
@@ -33,7 +33,7 @@ class Usuario(db.Model):
 class Evento(db.Model):
     __tablename__ = "evento"
 
-    id = db.Column(db.String(36), primary_key=True, autoincrement=True)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     usuario_id = db.Column(db.String(36), db.ForeignKey("usuario.id"), nullable=True)
     produto_id = db.Column(db.String(36), db.ForeignKey("produto.id"), nullable=True)
     tipo_evento = db.Column(db.String(50), nullable=True)
