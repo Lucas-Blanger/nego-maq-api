b685d8a5ed55b900cc6d4515da30f2b8776c5b8b
Lucas-Blanger <blangerlucas@gmail.com>
2025-11-10 13:20:35 -0300

    fix: enderecos do lado do admin

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,7 +4,6 @@ from routes.admin.pedidos_admin import admin_pedidos_routes
 from routes.public import public_routes
 from routes.public.pedidos_public import public_routes_pedidos
 from routes.public.auth import auth_routes
-from routes.admin.enderecos_admin import admin_enderecos_routes
 from routes.public.enderecos_public import public_enderecos_routes
 from routes.public.melhor_envio import frete_routes
 from routes.public.pagamento import pagamentos_routes
@@ -16,7 +15,6 @@ bp.register_blueprint(admin_pedidos_routes)
 bp.register_blueprint(public_routes)
 bp.register_blueprint(public_routes_pedidos)
 bp.register_blueprint(auth_routes)
-bp.register_blueprint(admin_enderecos_routes)
 bp.register_blueprint(public_enderecos_routes)
 bp.register_blueprint(frete_routes)
 bp.register_blueprint(pagamentos_routes)
